4f75a1febb52be9ff91d0365809050bc3c133b9e
kartnyk <71129045+kartnyk@users.noreply.github.com>
2023-10-25 12:49:47 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -4,16 +4,18 @@ const rds = new AWS.RDSDataService({ region: 'us-east-1' }); // Replace 'us-east
 exports.handler = async (event) => {
     // Replace these with your actual Aurora MySQL database credentials and connection details
     const dbClusterArn = 'arn:aws:rds:us-east-1:123456789012:cluster:your-cluster-name';
+    const masterUsername = 'your-master-username';
+    const masterPassword = 'your-master-password';
     const dbName = 'your-database-name';
-    const dbSecretArn = 'arn:aws:secretsmanager:us-east-1:123456789012:secret:your-secret-name';
     const sqlStatement = 'SELECT * FROM your_table_name'; // Replace with your SQL query
 
     try {
         const params = {
-            awsSecretStoreArn: dbSecretArn,
             dbClusterOrInstanceArn: dbClusterArn,
-            sqlStatements: sqlStatement,
+            secretArn: 'arn:aws:secretsmanager:us-east-1:123456789012:secret:your-secret-name',
             database: dbName,
+            sql: sqlStatement,
+            resourceArn: dbClusterArn,
             includeResultMetadata: true
         };
 
